990c584fe0e5e8d9ef9a885eb021058d7a60a4a1
biubiubiu23314 <43579393+biubiubiu23314@users.noreply.github.com>
2019-03-29 17:17:30 +0800

    Add files via upload

---
+++ dreamimg.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var Jimp = require('jimp');
+
+const MaskSizex = 195;
+const MaskSizey = 130;
+const OffsetX = 0;
+const OffsetY = 0;
+
+// Read background image
+var backgroundImage = null;
+Jimp.read('drm_background.png').then(image => {
+    backgroundImage = image;
+});
+Jimp.read('drm_cloudMask.png').then(image => {
+    cloudMask = image;
+});
+
+var dreamImage = function(inputImage, agent = null) {
+    return new Promise(resolve => {
+        Jimp.read({
+            url: inputImage,
+            agent: agent
+        }).then(foreground => {
+            var composed = backgroundImage.clone();
+            foreground.scaleToFit(MaskSizex, MaskSizey);
+            foreground.contain(MaskSizex, MaskSizey);
+            var cloud = foreground.clone();
+            cloud.mask(cloudMask);
+            composed.composite(cloud, OffsetX, OffsetY - Diff);
+            composed.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+                if (err) {
+                    throw err;
+                }
+                resolve(buffer);
+            });
+        }).catch(err => {
+            console.error(err);
+            resolve(null);
+        })
+    });
+}
+
+if (require.main === module) {
+    setTimeout(() => {
+        dreamImage('google.png').then(buffer => {
+            fs.writeFileSync('test.png', buffer);
+        })
+    }, 100);
+}
+
+exports.dreamImage = dreamImage;
---
+++ index.js
@@ -0,0 +1,156 @@
+const BrkImg = require('./brkimg.js');
+const DrmImg = require('./dreamimg')
+const Telegraf = require('telegraf');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint } = format;
+const SocksProxyAgent = require('socks-proxy-agent');
+
+const API_TOKEN = process.env.API_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        prettyPrint()
+    ),
+    transports: [
+        new transports.Console()
+    ]
+});
+
+var agent = null;
+if (process.env.SOCKS_PROXY) {
+    agent = new SocksProxyAgent(process.env.SOCKS_PROXY);
+}
+const options = {
+    telegram: {
+        agent: agent,
+        webhookReply: true
+    }
+};
+const bot = new Telegraf(API_TOKEN, options);
+
+bot.start(ctx => ctx.reply('Send image you want to break to me.'));
+bot.command('help', ctx => {
+    ctx.reply('Send image you want to break to me. It can be either photo or document.')
+});
+
+var dream = function(fileId, name, ctx){
+    return bot.telegram.getFileLink(fileId).then(fileLink => {
+        if (fileLink) {
+            DrmImg.dreamImage(fileLink, agent).then(result => {
+                if (result === null) {
+                    ctx.reply('Failed to read image.');
+                    return;
+                }
+                ctx.replyWithPhoto({ source: result , filename: `${name}.png` });
+                ctx.replyWithDocument({ source: result , filename: `${name}.png` });
+                logger.info(`[${ctx.message.from.id}] Replied.`);
+            });
+        }
+    });
+}
+
+bot.hears(/\/dream/,ctx =>{
+    ctx.reply(`Please send the image.`);
+    
+    bot.on('photo', ctx => {
+        logger.info(`[${ctx.message.from.id}] Sent a photo.`);
+        if (ctx.message.photo.length >= 2) {
+            var fileId = ctx.message.photo[1].file_id;
+            logger.info(`[${ctx.message.from.id}] File id: ${fileId}`);
+            dream(fileId, 'result', ctx).catch(err => {
+                logger.error(err);
+                ctx.reply(`Failed to download image.`);
+            });
+        }
+    });
+    
+    bot.on('document', ctx => {
+        logger.info(`[${ctx.message.from.id}] Sent a file.`);
+        var document = ctx.message.document;
+        if (document) {
+            var fileId = document.file_id;
+            logger.info(`[${ctx.message.from.id}] File id: ${fileId}`);
+            dream(fileId, `broken-${document.file_name.replace(/\.[^/.]+$/, '')}`, ctx).catch(err => {
+                logger.error(err);
+                ctx.reply(`Failed to download image.`);
+            });
+        }
+    });
+});
+
+var breakDocument = function(fileId, name, ctx) {
+    return bot.telegram.getFileLink(fileId).then(fileLink => {
+        if (fileLink) {
+            BrkImg.breakImage(fileLink, agent).then(result => {
+                if (result === null) {
+                    ctx.reply('Failed to read image.');
+                    return;
+                }
+                ctx.replyWithPhoto({ source: result , filename: `${name}.png` });
+                ctx.replyWithDocument({ source: result , filename: `${name}.png` });
+                logger.info(`[${ctx.message.from.id}] Replied.`);
+            });
+        }
+    });
+}
+
+bot.hears(/\/breakimg/,ctx =>{
+    ctx.reply(`Please send the image.`);
+
+    bot.on('photo', ctx => {
+        logger.info(`[${ctx.message.from.id}] Sent a photo.`);
+        if (ctx.message.photo.length >= 2) {
+            var fileId = ctx.message.photo[1].file_id;
+            logger.info(`[${ctx.message.from.id}] File id: ${fileId}`);
+            breakDocument(fileId, 'result', ctx).catch(err => {
+                logger.error(err);
+                ctx.reply(`Failed to download image.`);
+            });
+        }
+    });
+    
+    bot.on('document', ctx => {
+        logger.info(`[${ctx.message.from.id}] Sent a file.`);
+        var document = ctx.message.document;
+        if (document) {
+            var fileId = document.file_id;
+            logger.info(`[${ctx.message.from.id}] File id: ${fileId}`);
+            breakDocument(fileId, `broken-${document.file_name.replace(/\.[^/.]+$/, '')}`, ctx).catch(err => {
+                logger.error(err);
+                ctx.reply(`Failed to download image.`);
+            });
+        }
+    });
+});
+
+bot.hears(/\/breakme/, ctx => {
+    logger.info(`[${ctx.message.from.id}] /breakme`);
+    bot.telegram.getUserProfilePhotos(ctx.message.from.id).then(userProfilePhotos => {
+        var photos = userProfilePhotos.photos;
+        var username = ctx.message.from.username;
+        if (typeof photos !== 'undefined' && photos.length > 0 && photos[0].length > 0) {
+            var fileId = photos[0][0].file_id;
+            if (!username) {
+                username = 'result';
+            }
+            logger.info(`[${ctx.message.from.id}] Profile photo file id: ${fileId}`);
+            breakDocument(fileId, username, ctx).catch(err => {
+                logger.error(err);
+                ctx.reply(`Failed to download profile image for ${username}.`);
+            });
+        } else {
+            ctx.reply(`User ${username} does not have profile photos.`);
+        }
+    }).catch(err => {
+        logger.error(err);
+        ctx.reply(`Failed to fetch profile image for ${username}.`);
+    })
+});
+
+//bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+//bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+
+bot.launch();
