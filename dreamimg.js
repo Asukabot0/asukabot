7437c76a89db458cc02c415e614b46be042c1942
biubiubiu23314 <43579393+biubiubiu23314@users.noreply.github.com>
2019-03-29 23:59:13 +0800

    Update dreamimg.js

--- dreamimg.js
+++ dreamimg.js
@@ -26,8 +26,8 @@ var dreamImage = function(inputImage, agent = null) {
             foreground.contain(MaskSizex, MaskSizey);
             var cloud = foreground.clone();
             cloud.mask(cloudMask);
-            composed.composite(cloud, OffsetX, OffsetY);
-            composed.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+            cloud.composite(composed, OffsetX, OffsetY);
+            cloud.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
                 if (err) {
                     throw err;
                 }
